51a839bcb4fc389ef288b2819df17eb1ff49d127
TheBlueGenie <thebluegenieuk@gmail.com>
2024-04-08 23:00:33 +0100

    add basic splash, title and end game screens.

--- src/main.c
+++ src/main.c
@@ -1,61 +1,170 @@
+// vim:fileencoding=utf-8:foldmethod=marker
+
+// Defines and includes {{{
+#include <math.h>
 #include <raylib.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define SCR_WIDTH 800
-#define SCR_HEIGHT 600
-#define TITLE "Image Grid"
+// TODO: Add player, art and music, score system;
+
+#define SCR_WIDTH 600
+#define SCR_HEIGHT 700
+#define CAPTION "Catcher"
 #define FPS 60
 #define SPRITE_SIZE 64
-#define NUM_ITEMS 20
+#define NUM_ITEMS 10
 #define MIN_Y_SPEED 200
 #define MAX_Y_SPEED 400
+#define SPAWN_TIME 1.5f
+#define SPAWN_POS -50
+#define PLAYER_LIVES 3
+// }}}
 
-enum itemType {
-  bumNote,
-  note,
-  violin,
-  viola,
-  cello,
-  dblBass,
-  NumOfTypes,
-};
+// Declarations {{{
+typedef enum State { SPLASH, TITLE, PLAYING, ENDGAME } State;
+
+typedef enum ItemType {
+  BUMNOTE,
+  NOTE,
+  VIOLIN,
+  VIOLA,
+  CELLO,
+  DBLBASS,
+  NUMOFTYPES,
+} ItemType;
 
 typedef struct Item {
   Vector2 pos;
   int ySpeed;
   bool active;
-  enum itemType type;
+  enum ItemType type;
 } Item;
+
+typedef struct Player {
+  Vector2 pos;
+  int lives;
+  int score;
+} Player;
 
 typedef struct Game {
   int numItems;
   Item items[NUM_ITEMS];
+  Player player;
+  bool gameOver;
 } Game;
 
+// Function declarations
 void init(Game *game);
+void spawn(Game *game);
 void update(Game *game);
+void draw(Game *game);
 void drawItems(Game *game);
 int getRandom(int min, int max);
+// }}}
 
 int main(void) {
+  InitWindow(SCR_WIDTH, SCR_HEIGHT, CAPTION); // Create Window
+  SetTargetFPS(FPS);                          // Set FPS
+  State currentState = SPLASH;
   srand(time(NULL));
-  InitWindow(SCR_WIDTH, SCR_HEIGHT, TITLE);
-  SetTargetFPS(FPS);
-
   Game game = {};
-  init(&game);
+  double spawnTimer = 0;
+  int framesCounter = 0;
 
   while (!WindowShouldClose()) {
+
+    switch (currentState) {
+    case SPLASH:
+      framesCounter++; // Count frames
+      // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+      if (framesCounter > 120) {
+        currentState = TITLE;
+      }
+      break;
+
+    case TITLE:
+      if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
+        currentState = PLAYING;
+        init(&game);
+      }
+      break;
+
+    case PLAYING:
+      if (game.gameOver) {
+        currentState = ENDGAME;
+      }
+
+      if (IsKeyPressed(KEY_X)) {
+        currentState = ENDGAME;
+      }
+      break;
+
+    case ENDGAME:
+      if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
+        currentState = TITLE;
+      }
+      break;
+    }
 
     update(&game);
 
     BeginDrawing();
     ClearBackground(BLACK);
 
-    drawItems(&game);
+    switch (currentState) {
+    case SPLASH:
+      DrawText(CAPTION, (int)(GetScreenWidth() - MeasureText(CAPTION, 30)) / 2,
+               350, 30, SKYBLUE);
+      break;
+
+    case TITLE:
+      DrawText(CAPTION, (int)(GetScreenWidth() - MeasureText(CAPTION, 30)) / 2,
+               10, 30, SKYBLUE);
+      DrawText(
+          "Press Enter To Start",
+          (int)(GetScreenWidth() - MeasureText("Press Enter To Start", 30)) / 2,
+          600, 30, SKYBLUE);
+      break;
+
+    case PLAYING:
+
+      draw(&game);
+
+      spawnTimer = spawnTimer + GetFrameTime();
+      if (spawnTimer >= SPAWN_TIME) {
+        spawn(&game);
+        spawnTimer = 0;
+      }
+      break;
+
+    case ENDGAME:
+      DrawText(CAPTION, (int)(GetScreenWidth() - MeasureText(CAPTION, 30)) / 2,
+               10, 30, SKYBLUE);
+      DrawText("Game Over",
+               (int)(GetScreenWidth() - MeasureText("Game Over", 30)) / 2, 150,
+               30, SKYBLUE);
+      DrawText("Score", (int)(GetScreenWidth() - MeasureText("Score", 30)) / 2,
+               300, 30, SKYBLUE);
+      DrawText(TextFormat("%d", game.player.score),
+               (int)(GetScreenWidth() -
+                     MeasureText(TextFormat("%d", game.player.score), 30)) /
+                   2,
+               340, 30, SKYBLUE);
+      DrawText(
+          "Press Enter To Continue",
+          (int)(GetScreenWidth() - MeasureText("Press Enter To Continue", 30)) /
+              2,
+          600, 30, SKYBLUE);
+
+      break;
+
+    default:
+      break;
+    }
+
     EndDrawing();
   }
 
@@ -63,27 +172,109 @@ int main(void) {
   return 0;
 }
 
+// init {{{
 void init(Game *game) {
   game->numItems = NUM_ITEMS;
+  game->gameOver = false;
+  game->player.pos.x = (GetScreenWidth() - SPRITE_SIZE) / 2.0f;
+  game->player.pos.y = (GetScreenHeight() - SPRITE_SIZE * 1.1f);
+  game->player.lives = PLAYER_LIVES;
 
+  // Initialize game items
   for (int i = 0; i < game->numItems; ++i) {
     game->items[i].pos.x = rand() % (SCR_WIDTH - SPRITE_SIZE);
-    game->items[i].pos.y = 50;
-    game->items[i].type = rand() % NumOfTypes;
+    game->items[i].pos.y = SPAWN_POS;
+    game->items[i].type = rand() % NUMOFTYPES;
     game->items[i].ySpeed = getRandom(MIN_Y_SPEED, MAX_Y_SPEED);
-    game->items[i].active = true;
+    game->items[i].active = false;
   }
 }
+// }}}
 
-void update(Game *game) {
+// spawn {{{
+void spawn(Game *game) {
   for (int i = 0; i < game->numItems; ++i) {
-    game->items[i].pos.y += game->items[i].ySpeed * GetFrameTime();
-
-    if (game->items[i].pos.y > GetScreenHeight())
-      game->items[i].pos.y = -100;
+    if (game->items[i].active)
+      continue;
+    else {
+      game->items[i].pos.y = SPAWN_POS;
+      game->items[i].active = true;
+      break;
     }
+  }
 }
+// }}}
 
+// update {{{
+void update(Game *game) {
+
+  // Update Player
+  int dir = 0;
+  if (IsKeyDown(KEY_LEFT)) {
+    dir = -1;
+  }
+
+  if (IsKeyDown(KEY_RIGHT)) {
+    dir = 1;
+  }
+
+  game->player.pos.x += dir * 100 * GetFrameTime();
+
+  // Update Items
+  for (int i = 0; i < game->numItems; ++i) {
+    if (game->items[i].active) {
+      game->items[i].pos.y += game->items[i].ySpeed * GetFrameTime();
+
+      if (game->items[i].pos.y > GetScreenHeight()) {
+        game->items[i].pos.y = SPAWN_POS; // TODO: replace this magic number
+        continue;
+      }
+
+      // Check collisions
+      bool collision = false;
+      Rectangle itemBox = {game->items[i].pos.x, game->items[i].pos.y, SPRITE_SIZE, SPRITE_SIZE};
+      Rectangle playerBox = {game->player.pos.x, game->player.pos.y, SPRITE_SIZE, SPRITE_SIZE};
+
+      collision = CheckCollisionRecs(itemBox, playerBox);
+
+      if (collision) {
+        if (game->items[i].type == BUMNOTE) {
+          game->player.lives--;
+        } else {
+          game->player.score++;
+        }
+
+        printf("%i", game->items[i].type);
+        game->items[i].type = rand() % NUMOFTYPES;
+        game->items[i].active = false;
+      }
+
+      if (game->player.lives == 0) {
+        game->gameOver = true;
+      }
+    }
+  }
+}
+// }}}
+
+// draw {{{
+void draw(Game *game) {
+  drawItems(game);
+
+  // Draw Score
+  DrawText(TextFormat("Score: %d", game->player.score), 10, 10, 30, RAYWHITE);
+
+  // Draw Lives
+  int txtWidth = MeasureText(TextFormat("Score: %d", game->player.lives), 30);
+  DrawText(TextFormat("Lives: %d", game->player.lives), GetScreenWidth() - (txtWidth + 10), 10, 30, RAYWHITE);
+
+  // Draw Player
+  DrawRectangleV(game->player.pos, (Vector2){SPRITE_SIZE, SPRITE_SIZE},
+                 DARKBLUE);
+}
+// }}}
+
+// drawItems {{{
 void drawItems(Game *game) {
   for (int i = 0; i < game->numItems; ++i) {
     if (!game->items[i].active)
@@ -92,21 +283,21 @@ void drawItems(Game *game) {
     Color color;
 
     switch (game->items[i].type) {
-    case bumNote:
-      color = PURPLE;
+    case BUMNOTE:
+      color = RED;
       break;
-    case note:
+    case NOTE:
       color = GREEN;
       break;
-    case violin:
+    case VIOLIN:
       color = YELLOW;
       break;
-    case viola:
+    case VIOLA:
       color = ORANGE;
       break;
-    case cello:
+    case CELLO:
       color = PINK;
-    case dblBass:
+    case DBLBASS:
       color = BLUE;
     default:
       break;
@@ -116,6 +307,7 @@ void drawItems(Game *game) {
                    color);
   }
 }
+// }}}
 
 int getRandom(int min, int max) {
   return min + rand() / (RAND_MAX / (max - min + 1) + 1);
